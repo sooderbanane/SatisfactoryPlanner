3e452c0a33cdd5479f66b071dcbbbe1f299206d7
Erik <ewildpaner@lobos.ch>
2026-02-05 15:51:04 +0100

    first version works now

--- main.py
+++ main.py
@@ -70,6 +70,68 @@ def output_per_min_per_machine(item: str, recipes: dict) -> float:
     crafts_per_min = 60 / time_s
     return crafts_per_min * qty_out
 
+def expand_one_level(item :str, target_rate_per_minute: float, recipes: dict) -> dict:
+    if item not in recipes:
+        raise ValueError(f"No Recipe for '{item}'")
+
+    r = recipes[item]
+    time_s = r["time_s"]
+
+    output_per_min_one_machine = output_per_min_per_machine(item, recipes)
+    eq_machiens = target_rate_per_minute / output_per_min_one_machine
+
+    crafts_per_min_one_machine = 60 / time_s
+
+    needed = {} 
+    for in_item, qty_per_craft in r["input"].items():
+        needed[in_item] = crafts_per_min_one_machine * qty_per_craft * eq_machiens
+
+    return needed
+
+def merge_dicts(a: dict, b: dict) -> dict:
+    out = dict(a)
+    for k, v in b.items():
+        out[k] = out.get(k, 0.0) + v
+    return out 
+
+def requirements(
+        item: str,
+        target_rate_per_min: float,
+        recipes: dict,
+        stack: tuple[str, ...] = ()
+                 ) -> tuple[dict, dict]:
+    
+    if item in stack: 
+        raise ValueError(f"Recipe cycle detected: {' -> '.join(stack + (item,))}")
+
+    if item not in recipes:
+        return {item: target_rate_per_min}, {}
+    
+    recipe = recipes[item]
+
+    out_per_min_one_machine = output_per_min_per_machine(item, recipes)
+    eq_machines = target_rate_per_min / out_per_min_one_machine
+
+
+    machines = {recipe["produced_in"]: eq_machines}
+    
+    one_level = expand_one_level(item, target_rate_per_min, recipes)
+
+    raw_total = {}
+
+    machine_total = dict(machines)
+
+    for in_item, in_rate in one_level.items():
+        raw_sub, mach_sub = requirements(
+            in_item,
+            in_rate,
+            recipes,
+            stack + (item,)
+        )
+        raw_total = merge_dicts(raw_total, raw_sub)
+        machine_total = merge_dicts(machine_total, mach_sub)
+    return raw_total, machine_total 
+
 
 
 def main():
@@ -86,10 +148,21 @@ def main():
         print(f"{item}: {rate:.2f} / min per machine")
 
 
+    need = expand_one_level("reinforced_iron_plate", 10, recipes)
+    print(need)
+
     print(f"Loaded {len(recipes)} recipes from {path_recipes}")
     print(f"Loaded {len(buildings)} buildings from {path_buildings}")
     print("First recipe keys:", list(recipes.keys())[:5])
+    raw, machines = requirements("reinforced_iron_plate", 10, recipes)
 
+    print("\nRaw materials:")
+    for k, v in raw.items():
+        print(f"{k}: {v:.2f}/min")
+
+    print("\nMachines (ideal, fractional):")
+    for k, v in machines.items():
+        print(f"{k}: {v:.2f}")
 
 if __name__ == "__main__":
     main()
